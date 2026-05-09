3001e387bab4aabbb5316a3c3cd488f84e264906
Rushikesh-kumawat <rushikumawat2@gmail.com>
2026-05-09 06:23:22 +0000

    datatypes.js

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountemail = "hitesh@google.com"
+var accountPassword = "12345"
+let accountstate; 
+accountcity  = "jaipur"
+
+accountemail = "harsh@gmail.com"
+accountPassword = 151515
+accountcity = "bengaluru"
+
+
+console .log(accountId)
+
+console.table([accountId, accountPassword, accountemail, accountcity,accountstate])
+
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all as js code as newer version 
+
+// aleart (3+3) //we are using node js , not browser 
+
+console.log(3
+    +
+    3) // code relatibility should be high 
+
+    console.log("hitesh")
+
+
+let name = "hitesh"
+let age = 3
+
+
+
+//number => 2 to power 53
+// bigint 
+// string =>""
+// boolean => true/false 
+//null => standalone value 
+// underfined
+// symbol => unique
+
+
+//object
+
+console.log(typeof underfined);//underfined
+console.log(typeof null);//object   
