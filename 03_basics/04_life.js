bd514dd42f0a234be7036ebb471787f805fd765b
Rushikesh-kumawat <rushikumawat2@gmail.com>
2026-05-12 07:37:23 +0000

    04_life.js

---
+++ 03_basics/04_life.js
@@ -0,0 +1,11 @@
+//// Immediately Invocked Function Expressions ( IIFE )
+
+(function chai (){
+    console.log(`DB CONECTED`);
+
+})();
+
+((name) => {
+    console.log(`DB CONNECTED Two ${name}`);
+    
+})(`hitesh`)
