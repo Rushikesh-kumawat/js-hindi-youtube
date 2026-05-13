9c2aa6f90c1995fcd14c5bfd1e5edaf073a01dfb
Rushikesh-kumawat <rushikumawat2@gmail.com>
2026-05-13 08:56:46 +0000

    node 05_itirations/nine.js

---
+++ 05_itirations/five.js
@@ -0,0 +1,9 @@
+const coding = ["js", "ruby","java","paythan","cpp"]
+
+// coding.forEach(function(val){
+// console.log(val);
+
+
+// })
+
+c
---
+++ 05_itirations/three.js
@@ -0,0 +1,37 @@
+// for off
+
+
+
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr ) {
+// console.log(num);
+
+
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings){
+    // console.log(`each char is ${greet}`);
+    
+}
+// maps
+
+const map = new Map ()  
+map.set('IN',"India")
+map.set('USA, "United States Of America')
+map.set('Fr',"France")
+
+
+        // // console.log(map);
+
+        // for (const [key,value] of map ){
+        // console.log(key ,':-', value);
+
+        // }
+
+        // const myObject = {
+        // 'game1':
+
+        // }
